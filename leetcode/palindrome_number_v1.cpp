d85af180750ccfd19db849824fddae4e6376aff0
hotbig <randyn.yang@gmail.com>
2015-06-14 21:54:09 +0800

    palindrome_number

---
+++ leetcode/palindrome_number_v1.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class Solution{
+    public:
+        bool isPalindrome(int x){
+           // 111, 1, 121, -1, 12321 123321 22
+            int a = 0;
+            if(x < 0) x = (-1)*x;
+
+            while(x>a)
+            {
+                a = a*10 + (x%10);
+                x = x/10;
+            }
+            
+            return (x == a || (a/10)==x);
+        }
+};
+
+
+int main()
+{
+    Solution s;
+    cout << (s.isPalindrome(111) ? "yes":"no") << endl;
+    cout << (s.isPalindrome(1) ? "yes":"no") << endl;
+    cout << (s.isPalindrome(121) ? "yes":"no") << endl;
+    cout << (s.isPalindrome(-1) ? "yes":"no") << endl;
+    cout << (s.isPalindrome(12321) ? "yes":"no") << endl;
+    cout << (s.isPalindrome(123321) ? "yes":"no") << endl;
+    cout << (s.isPalindrome(22) ? "yes":"no") << endl;
+    
+    return 0;
+}
---
+++ leetcode/palindrome_number_v3.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class Solution{
+    public:
+        bool isPalindrome(int x){
+           // 111, 1, 121, -1, 12321 123321 22
+            int a = 0;
+            if(x < 0) return false;
+            if(!(x % 10) && (x/10)) return false;
+
+            while(x>a)
+            {
+                a = a*10 + (x%10);
+                x = x/10;
+            }
+            
+            return (x == a || (a/10)==x);
+        }
+};
+
+
+int main()
+{
+    Solution s;
+    cout << (s.isPalindrome(111) ? "yes":"no") << endl;
+    cout << (s.isPalindrome(1) ? "yes":"no") << endl;
+    cout << (s.isPalindrome(121) ? "yes":"no") << endl;
+    cout << (s.isPalindrome(-1) ? "yes":"no") << endl;
+    cout << (s.isPalindrome(12321) ? "yes":"no") << endl;
+    cout << (s.isPalindrome(123321) ? "yes":"no") << endl;
+    cout << (s.isPalindrome(22) ? "yes":"no") << endl;
+    
+    return 0;
+}
