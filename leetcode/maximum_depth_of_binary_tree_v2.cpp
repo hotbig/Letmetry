9e6a72bfcfc03656cd8d3d3e3b82ed7193645358
hotbig <randyn.yang@gmail.com>
2015-06-21 16:54:43 +0800

    maximum_depth_of_binary_tree_v

---
+++ leetcode/maximum_depth_of_binary_tree_v2.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+struct TreeNode{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x):val(x), left(NULL), right(NULL){}
+};
+
+
+class Solution{
+    public:
+        int maxDepth(TreeNode* root) {
+            int depth = 0;
+            if(root != NULL)
+            {
+                int left =  1+maxDepth(root->left);
+                int right = 1+maxDepth(root->right);
+
+                depth = left>right?left:right;
+            }
+            return depth;
+        }
+};
+
+
+int main()
+{
+    Solution s;
+    TreeNode a(0);
+    TreeNode b(-3);
+    TreeNode c(-9);
+    TreeNode d(-8);
+    TreeNode e(2);
+    TreeNode f(-5);
+    TreeNode g(5);
+    TreeNode h(-2);
+#if 0
+       1
+     2   5
+   3  4 6  7
+#endif 
+    a.left = &b;
+    a.right = &h;
+    b.left = &c;
+    b.right = &d;
+    c.left = &e;
+    e.left = &g;
+    d.right=&f;
+
+
+    
+    cout<< (s.maxDepth(&a)) << endl;;
+
+    return 0;
+}
