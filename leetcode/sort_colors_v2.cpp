3875409511f1d45b2a042eefa4934221e2f3ebb0
hotbig <randyn.yang@gmail.com>
2015-10-25 21:31:33 +0800

    sort_colors_v

---
+++ leetcode/sort_colors_v2.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<string>
+#include<vector>
+
+using namespace std;
+
+class Solution{
+    public:
+        void sortColors(vector<int>& nums) {
+            int c0=0, c1=0, c2=0;
+
+            for(int i = 0; i < nums.size(); i++){
+                if(nums[i] == 0) c0++;
+                if(nums[i] == 1) c1++;
+                if(nums[i] == 2) c2++;
+            }
+
+            for(int i = 0; i < nums.size(); i++){
+                if(i < c0)        {nums[i] = 0; continue;}
+                if(i < (c0+c1))   {nums[i] = 1; continue;}
+                if(i < (c0+c1+c2)){nums[i] = 2; continue;}
+            }        
+        }
+};
+
+
+int main()
+{
+    Solution s;
+    
+    return 0;
+}
